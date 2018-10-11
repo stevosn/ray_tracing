38754366f3adfd71b111e538b1730f4f30555a81
steves <steve.simmert@uni-tuebingen.de>
2018-10-11 16:48:47 +0200

    added entracne pupil calulation and drawing

--- ray_tracing.py
+++ ray_tracing.py
@@ -1,10 +1,16 @@
-from numpy import array, matrix, inf, nan, ones, zeros, arctan, isinf
+from numpy import array, matrix, inf, nan, ones, zeros, isinf, isfinite
 
 import matplotlib
 
 matplotlib.use('nbAgg')
 
 import matplotlib.pyplot as plt
+
+
+def get_image_pos(object_distance, focal_length):
+    s = object_distance
+    f = focal_length
+    return 1 / (1 / f - 1 / s)
 
 
 class OPE(object):
@@ -25,6 +31,7 @@ class OPE(object):
         """
         self.aperture = a
         self.name = name
+        self.focal_length = f
 
         if f:
             self.c = -1 / f
@@ -80,9 +87,24 @@ class OPE(object):
 
 
 def trace_ray(r, sequence):
-    """
-    Take r as input vector and trace the ray through the sequence of
+    """Take r as input vector and trace the ray through the sequence of
     optical path elements OLEs.
+
+    Arguments
+    ---------
+    r : 2-tuple
+        Vector with the first element beeing the ray height and the
+        second being the angle
+    sequence : list of optical path elements (OPE)
+
+    Returns
+    -------
+    (dist, ray)
+    dist : array
+        Array representing the position of the ray through the trace.
+    ray : array
+        Array representing the ray vectors at the corresponding
+        distances.
     """
     ray0 = zeros((2, len(sequence) + 1))
     ray0[:, 0] = r
@@ -128,7 +150,7 @@ def get_lens_pos(sequence):
 
     Returns
     -------
-    List of tuples with position and index of OPE in sequence.
+    List of tuples with index and position of OPE in sequence.
     """
     d = 0.0
     d_ = []
@@ -266,24 +288,25 @@ def draw_apertures(sequence, axis=None):
     else:
         ax = axis
 
-        plt_kws = dict(linewidth=2, linestyle='-',
-                       color='darkslategrey')
+    plt_kws = dict(linewidth=2, linestyle='-',
+                   color='darkslategrey')
 
     a_max = get_max_aperture(sequence)
 
-    # ylims = ax.get_ylim()
-    for idx, x in get_lens_pos(sequence):
-        a = sequence[idx].aperture
-        ax.plot([x, x], [-2*a_max, -a], **plt_kws)
-        ax.plot([x, x], [a, 2*a_max], **plt_kws)
-    # ax.set_ylim(ylims)
+    if a_max:
+        for idx, x in get_lens_pos(sequence):
+            a = sequence[idx].aperture
+            ax.plot([x, x], [-2*a_max, -a], **plt_kws)
+            ax.plot([x, x], [a, 2*a_max], **plt_kws)
 
 
 def get_max_aperture(sequence):
     """
     Return largest aperture in sequence.
+
+    Returns None if no apertures are defined.
     """
-    apertures = [ope.aperture for ope in sequence if ope.aperture != inf]
+    apertures = [abs(ope.aperture) for ope in sequence if ope.aperture != inf]
 
     if not apertures:
         out = None
@@ -345,6 +368,10 @@ class Trace(object):
 
         return axis
 
+    def reset_plot(self):
+        """ Reset internal plot axis."""
+        self._statics_drawn = False
+
     def plot_rays(self, h, parallel=False, d=None, axis=None,
                   label=None, plot_statics=True, **pltkws):
         """
@@ -352,8 +379,8 @@ class Trace(object):
 
         Arguments
         ---------
-        h : float
-            height of ray.
+        h : float or iterable
+            Height(s) of ray .
         sequence : list of OPE
             sequence of optical path elements.
         parallel : bool
@@ -372,36 +399,50 @@ class Trace(object):
         -------
         maplotlib.figure
         """
+        try:
+            heights = iter(h)
+        except TypeError:
+            heights = [h]
+
         if plot_statics:
             ax = self.plot_statics(axis=axis)
         else:
             ax = self.plot_axis
 
         if not any([a in pltkws for a in ['c', 'col', 'color']]):
+            cycle_colors = True
+
+        for h in heights:
+            if parallel:
+                d = d or 1.0
+                rin_0 = [h + d/2, 0.0]
+                rin_1 = [h - d/2, 0.0]
+            else:
+                if self.has_aperture():
+                    # get distance and aperture of first aperture
+                    _, d, aperture = get_first_aperture(self.sequence)
+                    a1, a2 = get_angle_lim(h, d, aperture)
+                    rin_0 = [h, a1]
+                    rin_1 = [h, a2]
+                else:
+                    try:
+                        _, d = get_lens_pos(self.sequence)[0]
+                    except:
+                        d = inf
+                    rin_0 = [h, 0]
+                    rin_1 = [h, -h/d]
+
+            dist, r0 = trace_ray(rin_0, self.sequence)
+            dist, r1 = trace_ray(rin_1, self.sequence)
+
+            max_y = max([max(abs(r0[0, :])), max(abs(r1[0, :]))])
+            self.set_max_y(max_y)
+
             pltkws['color'] = next(ax._get_lines.prop_cycler)['color']
 
-        if parallel:
-            d = d or 1.0
-            rin_0 = [h + d/2, 0.0]
-            rin_1 = [h - d/2, 0.0]
-        else:
-            # get distance and aperture of first aperture
-            _, d, aperture = get_first_aperture(self.sequence)
-
-            a1, a2 = get_angle_lim(h, d, aperture)
-            rin_0 = [h, a1]
-            rin_1 = [h, a2]
-
-        dist, r0 = trace_ray(rin_0, self.sequence)
-        dist, r1 = trace_ray(rin_1, self.sequence)
-
-        max_y = max([max(abs(r0[0, :])), max(abs(r1[0, :]))])
-        self.set_max_y(max_y)
-
-        #pltkws = {'linewidth': 0.5}.update(pltkws)
-
-        ax.plot(dist, r0[0, :], label=label or 'h={:1.2f}'.format(h), **pltkws)
-        ax.plot(dist, r1[0, :], **pltkws)
+            ax.plot(dist, r0[0, :],
+                    label=label or 'h={:1.2f}'.format(h), **pltkws)
+            ax.plot(dist, r1[0, :], **pltkws)
 
         return ax
 
@@ -415,3 +456,83 @@ class Trace(object):
 
         y = max([max_y, max_a])
         axis.set_ylim([-1.1*y, 1.1*y])
+
+    def get_idx_aperture_stop(self):
+        """
+        Find the optical element the defines the aperture stop of the
+        system.
+
+        Returns the index of the OPE in the sequence.
+        """
+        if get_max_aperture(self.sequence) is None:
+            print('No apertures found.')
+            out = None
+        else:
+            angle = 1e-10
+            while True:
+                _, rays = trace_ray((0, angle), self.sequence)
+                if all(isfinite(rays[0])):
+                    break
+                else:
+                    angle = angle / 2
+            ratio = 0.0
+            for idx, ope in enumerate(self.sequence):
+                ratio_ = rays[0, idx+1] / ope.aperture
+                if ratio_ > ratio:
+                    ratio = ratio_
+                    out = idx
+
+        return out
+
+    def has_aperture(self):
+        if get_max_aperture(self.sequence):
+            return True
+        else:
+            return False
+
+    def get_aperture_stop_position(self):
+        """
+        Reduce sequence upto aperture and get distance from lens pos
+        function.
+        """
+        _, d = get_lens_pos(
+            self.sequence[:self.get_idx_aperture_stop() + 1])[-1]
+
+        return d
+
+    def get_aperture_stop_size(self):
+        """
+        Return the (half) aperture stop size.
+        """
+        return self.sequence[self.get_idx_aperture_stop()].aperture
+
+    def calc_entrance_pupil_position(self):
+        """ sequence of OPEs preceeding the aperture stop """
+        sequence_prec = self.sequence[:self.get_idx_aperture_stop()]
+        d_ap = self.get_aperture_stop_position()
+        x = d_ap
+        for idx, lens_pos in get_lens_pos(sequence_prec)[::-1]:
+            # object distance (reverse ray self (yields negative d_obj))
+            d_obj = lens_pos - x
+            # image distance
+            d_img = get_image_pos(d_obj, sequence_prec[idx].focal_length)
+            x = lens_pos + d_img
+
+        return x
+
+    def draw_entrance_pupil(self, axis=None):
+        """
+        Draw the apparent entrance pupil.
+        """
+        if axis:
+            ax = axis
+        else:
+            ax = self.plot_axis
+        x = self.calc_entrance_pupil_position()
+        a = self.get_aperture_stop_size()
+        y_max = get_max_aperture(self.sequence) if get_max_aperture(
+            self.sequence) else 2 * a
+
+        plt_kws = dict(linewidth=2, linestyle='-', color='slategrey')
+        ax.plot([x, x], [a, y_max], **plt_kws)
+        ax.plot([x, x], [-y_max, -a], **plt_kws)
